7c2ff58a258c056a480322f38cc93915a00a419b
Ashwin <ash.sponge@email.com>
2025-12-08 01:34:54 -0500

    more changes

--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -1,25 +1,47 @@
-import { v4 as uuidv4 } from "uuid";
+import model from "./model.js";
 export default function EnrollmentsDao(db) {
-  function enrollUserInCourse(userId, courseId) {
-    const { enrollments } = db;
-    const newEnrollment = { _id: uuidv4(), user: userId, course: courseId };
-enrollments.push(newEnrollment);
-return newEnrollment;
-  }
+  
+ /* async function findCoursesForUser(userId) {
+   const enrollments = await model.find({ user: userId }).populate("course");
+   return enrollments.map((enrollment) => enrollment.course);
+ } */
 
-  function unenrollUserFromCourse(userId, courseId) {
-    const { enrollments } = db;
-    db.enrollments = enrollments.filter(
-  (enrollment) => !(enrollment.user === userId && enrollment.course === courseId)
-);
-return db.enrollments;
-  }
-
-  function findEnrollmentsForUser(userId) {
-  return db.enrollments.filter(e => e.user === userId);
+async function findCoursesForUser(userId) {
+ const enrollments = await model.find({ user: userId }).populate("course");
+ return enrollments.map((enrollment) => enrollment.course);
 }
 
-  
-  return { enrollUserInCourse, unenrollUserFromCourse, findEnrollmentsForUser};
+ async function findUsersForCourse(courseId) {
+   const enrollments = await model.find({ course: courseId }).populate("user");
+   return enrollments.map((enrollment) => enrollment.user);
+ }
+
+
+ function enrollUserInCourse(userId, courseId) {
+   return model.create({
+     user: userId,
+     course: courseId,
+     _id: `${userId}-${courseId}`,
+   });
+ }
+ function unenrollUserFromCourse(user, course) {
+   return model.deleteOne({ user, course });
+ }
+
+ 
+
+ function unenrollAllUsersFromCourse(courseId) {
+   return model.deleteMany({ course: courseId });
+ }
+
+
+ return {
+   findCoursesForUser,
+   findUsersForCourse,
+   enrollUserInCourse,
+   unenrollUserFromCourse,
+   unenrollAllUsersFromCourse,
+
+ };
 }
 
--- Kambaz/Enrollments/routes.js
+++ Kambaz/Enrollments/routes.js
@@ -21,6 +21,8 @@ export default function EnrollmentRoutes(app, db) {
         const enrollments = dao.findEnrollmentsForUser(userId);
         res.json(enrollments);
     };
+
+    
     app.post("/api/courses/enrollments/:courseId/enroll", enrollUserInCourse);
     app.delete("/api/courses/enrollments/:courseId/unenroll", unenrollUserFromCourse);
     app.get("/api/users/enrollments/:userId/enrollments", findEnrollmentsForUser);
